186f13c5be1b335eabaa175ea4aa186246565fea
maicolswss11 <michele.poerio1109@gmail.com>
2026-06-14 14:21:31 +0200

    chore: switch production to self-hosted Supabase

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,15 @@
-window.TFV_CONFIG = Object.freeze({
+/* window.TFV_CONFIG = Object.freeze({
   supabaseUrl: "https://edbmlipovuvyvwrcnfxu.supabase.co",
   supabasePublishableKey: "sb_publishable_Qobgp_Eu4fvDditiK9hNLg_nPTxHNVR"
-});
+}); */ 
+
+
+window.TFV_CONFIG = Object.freeze({
+  supabaseUrl: "https://desktop-chun4r5.tail43c3e4.ts.net",
+  supabasePublishableKey: "sb_publishable_86HTkJfIzPa3Mh9mo43TS2_b62K-LJC"
+}); 
+
+
+
+
+
